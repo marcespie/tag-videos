e49e0264a9392ac2bef43138c90124740cdc7e57
Marc Espie <espie@nausicaa.home>
2024-08-26 09:28:57 +0200

    add a view to the schema allow schema to alter tables and views add a "viewtag" sh utility that can the intersection of a few tags

--- mydb.sql
+++ mydb.sql
@@ -1,4 +1,12 @@
-CREATE TABLE file (Id integer primary key, path text unique on conflict ignore);
-CREATE TABLE tag (Id integer primary key, tag text unique on conflict ignore);
-CREATE TABLE filetag (Id integer primary key, FileId integer, TagId integer, 
+CREATE TABLE if not exists file 
+	(Id integer primary key, path text unique on conflict ignore);
+CREATE TABLE if not exists tag 
+	(Id integer primary key, tag text unique on conflict ignore);
+CREATE TABLE if not exists filetag 
+	(Id integer primary key, FileId integer, TagId integer, 
 	constraint nodups unique (FileId, TagId) on conflict ignore);
+create view _filetag as
+	select tag.tag as tag, file.path as path
+	from filetag
+	    join tag on filetag.tagid=tag.id
+	    join file on filetag.fileid=file.id;
