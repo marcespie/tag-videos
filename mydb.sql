fc34fbe38e7f4a448169540b504ee4796fac7880
Marc Espie <espie@openbsd.org>
2024-08-27 16:20:23 +0200

    add LICENSE everywhere
    as is customary in the BSD world

--- mydb.sql
+++ mydb.sql
@@ -1,3 +1,20 @@
+-- Copyright (c) 2024 Marc Espie <espie@openbsd.org>
+-- 
+-- Permission to use, copy, modify, and distribute this software for any
+-- purpose with or without fee is hereby granted, provided that the above
+-- copyright notice and this permission notice appear in all copies.
+-- 
+-- THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+-- WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+-- MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+-- ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+-- WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+-- ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+-- OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+--
+
+-- XXX maybe it would be better to have a directory/file separation
+-- together with a file view ?
 CREATE TABLE if not exists file 
 	(Id integer primary key, path text unique on conflict ignore);
 CREATE TABLE if not exists tag 
@@ -5,6 +22,7 @@ CREATE TABLE if not exists tag
 CREATE TABLE if not exists filetag 
 	(Id integer primary key, FileId integer, TagId integer, 
 	constraint nodups unique (FileId, TagId) on conflict ignore);
+-- just in case one wants to check
 create view _filetag as
 	select tag.tag as tag, file.path as path
 	from filetag
